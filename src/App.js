eddb0d28fc5525fcabdb60daef1dd116405693a9
Dhrunit <jimmyprajapati99@gmail.com>
2021-04-09 19:31:29 +0530

    Added user login functionality using redux hooks and also set up a register route

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import LoginScreen from './screens/LoginScreen'
 function App() {
 	return (
 		<Router>
 			<Header />
 			<main className='py-3'>
 				<Container>
+					<Route path='/login' component={LoginScreen} />
 					<Route path='/' exact component={HomeScreen} />
 					<Route path='/product/:id' component={ProductScreen} />
+					<Route path='/register' component={RegisterScreen} />
 					<Route path='/cart/:id?' component={CartScreen} />
+					<Route path='/' component={HomeScreen} exact />
 				</Container>
 			</main>
 			<Footer />
