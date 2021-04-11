41a7482661af8e10b4080804ae1d5599467c22e0
Dhrunit <jimmyprajapati99@gmail.com>
2021-04-11 11:11:03 +0530

    Added Update Profile workflow

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import LoginScreen from './screens/LoginScreen'
+import ProfileScreen from './screens/ProfileScreen'
 function App() {
 	return (
 		<Router>
@@ -16,6 +17,7 @@ function App() {
 					<Route path='/login' component={LoginScreen} />
 					<Route path='/' exact component={HomeScreen} />
 					<Route path='/product/:id' component={ProductScreen} />
+					<Route path='/profile' component={ProfileScreen} />
 					<Route path='/register' component={RegisterScreen} />
 					<Route path='/cart/:id?' component={CartScreen} />
 					<Route path='/' component={HomeScreen} exact />
--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
+import {
+	userLoginReducer,
+	userRegisterReducer,
+	userDetailsReducer,
+	userUpdateProfileReducer,
+} from './reducers/userReducers'
 import {
 	productListReducer,
 	productDetailsReducer,
@@ -14,6 +19,8 @@ const reducer = combineReducers({
 	cart: cartReducer,
 	userLogin: userLoginReducer,
 	userRegister: userRegisterReducer,
+	userDetails: userDetailsReducer,
+	userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
